1a3e8743857bae47981fba05814d0a20efbdb693
mrgromov <mailrxtx@gmail.com>
2019-06-18 01:29:18 +0400

    redux stream CRUD implemented

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,24 @@
+import { omit, mapKeys } from "lodash"
+import { CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM } from "../actions/types"
+
+export default (state = {}, action) => {
+	switch (action.type) {
+		case FETCH_STREAMS:
+			return { ...state, ...mapKeys(action.payload, "id") }
+
+		case FETCH_STREAM:
+			return { ...state, [action.payload.id]: action.payload }
+
+		case CREATE_STREAM:
+			return { ...state, [action.payload.id]: action.payload }
+
+		case EDIT_STREAM:
+			return { ...state, [action.payload.id]: action.payload }
+
+		case DELETE_STREAM:
+			return omit(state, action.payload)
+
+		default:
+			return state
+	}
+}
