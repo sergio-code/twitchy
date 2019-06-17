6cc80d075432ad483492a667ed0f4d68276be9b7
mrgromov <mailrxtx@gmail.com>
2019-06-18 01:26:41 +0400

    Router updated to use external history

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
 import React from "react"
-import { BrowserRouter, Route } from "react-router-dom"
+import { Router, Route } from "react-router-dom"
 import Header from "./Header"
 import { StreamCreate, StreamDelete, StreamEdit, StreamList, StreamShow } from "./streams"
+import history from "../history"
 
 const App = () => (
 	<div className="ui container">
-		<BrowserRouter>
+		<Router history={history}>
 			<div>
 				<Header />
 				<Route path="/" exact component={StreamList} />
 				<Route path="/streams/new" exact component={StreamCreate} />
-				<Route path="/streams/edit" exact component={StreamEdit} />
-				<Route path="/streams/delete" exact component={StreamDelete} />
-				<Route path="/streams/show" exact component={StreamShow} />
+				<Route path="/streams/edit/:id" exact component={StreamEdit} />
+				<Route path="/streams/delete/:id" exact component={StreamDelete} />
+				<Route path="/streams/show/:id" exact component={StreamShow} />
 			</div>
-		</BrowserRouter>
+		</Router>
 	</div>
 )
 
