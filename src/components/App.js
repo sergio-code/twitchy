b15fc29cd7e5ad436bf6f9a786f39cc20c1efcc8
mrgromov <mailrxtx@gmail.com>
2019-06-19 00:35:56 +0400

    Router Switch is added

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,38 @@
-import React from "react"
-import { Router, Route } from "react-router-dom"
-import Header from "./Header"
-import { StreamCreate, StreamDelete, StreamEdit, StreamList, StreamShow } from "./streams"
-import history from "../history"
+import React from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
+import Header from './Header'
+import {
+    StreamCreate,
+    StreamDelete,
+    StreamEdit,
+    StreamList,
+    StreamShow
+} from './streams'
+import history from '../history'
 
 const App = () => (
-	<div className="ui container">
-		<Router history={history}>
-			<div>
-				<Header />
-				<Route path="/" exact component={StreamList} />
-				<Route path="/streams/new" exact component={StreamCreate} />
-				<Route path="/streams/edit/:id" exact component={StreamEdit} />
-				<Route path="/streams/delete/:id" exact component={StreamDelete} />
-				<Route path="/streams/show/:id" exact component={StreamShow} />
-			</div>
-		</Router>
-	</div>
+    <div className="ui container">
+        <Router history={history}>
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route
+                        path="/streams/edit/:id"
+                        exact
+                        component={StreamEdit}
+                    />
+                    <Route
+                        path="/streams/delete/:id"
+                        exact
+                        component={StreamDelete}
+                    />
+                    <Route path="/streams/:id" exact component={StreamShow} />
+                </Switch>
+            </div>
+        </Router>
+    </div>
 )
 
 export default App
