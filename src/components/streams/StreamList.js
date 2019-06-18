c182bb934caf9e5376d63005d7238a931c1a7a9e
mrgromov <mailrxtx@gmail.com>
2019-06-18 21:40:27 +0400

    delete button style updated

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,75 +1,82 @@
-import React from "react"
-import { connect } from "react-redux"
-import { fetchStreams } from "../../actions"
-import { Link } from "react-router-dom"
+import React from 'react'
+import { connect } from 'react-redux'
+import { fetchStreams } from '../../actions'
+import { Link } from 'react-router-dom'
 
 class StreamList extends React.Component {
-	componentDidMount() {
-		this.props.fetchStreams()
-	}
+    componentDidMount() {
+        this.props.fetchStreams()
+    }
 
-	renderAdmin(stream) {
-		if (stream.userId === this.props.currentUserId) {
-			return (
-				<div className="right floated content">
-					<Link to={`/streams/edit/${stream.id}`} className="ui button primary">
-						Edit
-					</Link>
-					<Link to={`/streams/delete/${stream.id}`} className="ui button primary">
-						Delete
-					</Link>
-				</div>
-			)
-		}
-	}
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <Link
+                        to={`/streams/delete/${stream.id}`}
+						title="Delete Stream"
+                        className="ui icon button grey"
+                    >
+                        <i className="icon trash alternate" />
+                    </Link>
+                    <Link
+						to={`/streams/edit/${stream.id}`}
+                        className="ui button primary"
+                    >
+                        Edit
+                    </Link>
+                </div>
+            )
+        }
+    }
 
-	renderList() {
-		return this.props.streams.map(stream => {
-			return (
-				<div className="item" key={stream.id}>
-					{this.renderAdmin(stream)}
-					<i className="large middle aligned icon camera" />
-					<div className="content">
-						{stream.title}
-						<div className="description">{stream.description}</div>
-					</div>
-				</div>
-			)
-		})
-	}
+    renderList() {
+        return this.props.streams.map((stream) => {
+            return (
+                <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">{stream.description}</div>
+                    </div>
+                </div>
+            )
+        })
+    }
 
-	renderCreate() {
-		if (this.props.isSignedIn) {
-			return (
-				<div style={{ textAlign: "right" }}>
-					<Link to="/streams/new" className="ui button primary">
-						Create Stream
-					</Link>
-				</div>
-			)
-		}
-	}
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
 
-	render() {
-		return (
-			<div>
-				<h2>Streams</h2>
-				<div className="ui celled list">{this.renderList()}</div>
-				{this.renderCreate()}
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
+            </div>
+        )
+    }
 }
 
-const masStateToProps = state => {
-	return {
-		streams: Object.values(state.streams),
-		currentUserId: state.auth.userId,
-		isSignedIn: state.auth.isSignedIn
-	}
+const masStateToProps = (state) => {
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    }
 }
 
 export default connect(
-	masStateToProps,
-	{ fetchStreams }
+    masStateToProps,
+    { fetchStreams }
 )(StreamList)
