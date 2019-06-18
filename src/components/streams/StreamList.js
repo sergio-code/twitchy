b90231abefe7a04b87245ca7a36935a38f0caebb
mrgromov <mailrxtx@gmail.com>
2019-06-19 00:36:49 +0400

    Per stream Link added

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -14,13 +14,13 @@ class StreamList extends React.Component {
                 <div className="right floated content">
                     <Link
                         to={`/streams/delete/${stream.id}`}
-						title="Delete Stream"
+                        title="Delete Stream"
                         className="ui icon button grey"
                     >
                         <i className="icon trash alternate" />
                     </Link>
                     <Link
-						to={`/streams/edit/${stream.id}`}
+                        to={`/streams/edit/${stream.id}`}
                         className="ui button primary"
                     >
                         Edit
@@ -37,7 +37,7 @@ class StreamList extends React.Component {
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
                         <div className="description">{stream.description}</div>
                     </div>
                 </div>
